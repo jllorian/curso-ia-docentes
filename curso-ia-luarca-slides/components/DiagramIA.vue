c75b80b7b7005101896edfa7314a16f70caaeda6
José María Llorián Álvarez <josellorian@gmail.com>
2025-04-07 20:05:16 +0200

    Actualizar enlaces de navegación en grupos interactivos del diagrama

--- curso-ia-luarca-slides/components/DiagramIA.vue
+++ curso-ia-luarca-slides/components/DiagramIA.vue
@@ -62,7 +62,7 @@
         </defs>
   
         <!-- Grupos interactivos -->
-        <g class="interactive-group" transform="translate(600, 99)" @click="navigateToSlide(7)">
+        <g class="interactive-group" transform="translate(600, 99)" @click="navigateToSlide(21)">
           <use href="#4.cu_sy_dl2hha1q79o21-fill"/>
           <use href="#4.cu_sy_dl2hha1q79o21-stroke"/>
           <use href="#tx_intelige_m7yuim1q7b40a-fill" x="-250"/>
@@ -70,7 +70,7 @@
           <image href="../recursos/venn/vennIA.svg" x="250" y="0" width="100" height="100"/>
         </g>
   
-        <g class="interactive-group" transform="translate(672, 243)" @click="navigateToSlide(7)">
+        <g class="interactive-group" transform="translate(672, 243)" @click="navigateToSlide(21)">
           <use href="#3.cu_sy_958zcu1q79oub-fill"/>
           <use href="#3.cu_sy_958zcu1q79oub-stroke"/>
           <use href="#tx_aprendiz_8x4t8e1q7b2fg-fill" x="-302"/>
@@ -78,7 +78,7 @@
           <image href="../recursos/venn/vennML.svg" x="177" y="0" width="100" height="100"/>
         </g>
   
-        <g class="interactive-group" transform="translate(744, 387)" @click="navigateToSlide(7)">
+        <g class="interactive-group" transform="translate(744, 387)" @click="navigateToSlide(21)">
           <use href="#2.cu_sy_9lz3y1q79pn1-fill"/>
           <use href="#2.cu_sy_9lz3y1q79pn1-stroke"/>
           <use href="#tx_aprendiz_4hbb3y1q7b37q-fill" x="-390"/>
@@ -86,7 +86,7 @@
           <image href="../recursos/venn/vennDL.svg" x="104" y="0" width="100" height="100"/>
         </g>
   
-        <g class="interactive-group" transform="translate(820, 531)" @click="navigateToSlide(8)">
+        <g class="interactive-group" transform="translate(820, 531)" @click="navigateToSlide(22)">
           <use href="#1.cu_sy_1uuwiym1q79q87-fill"/>
           <use href="#1.cu_sy_1uuwiym1q79q87-stroke"/>
           <use href="#tx_iagenera_1hszi1q7b400-fill" x="-520"/>
